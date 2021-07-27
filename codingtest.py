63482ed61121182c6343c7ce4e2b1dd59d011218
jjo <k01066624566@gmail.com>
2021-07-27 20:40:37 +0900

    update

--- codingtest.py
+++ codingtest.py
@@ -5,10 +5,8 @@ for i in range(n):
     arr[i][0],arr[i][1]=input().split()
     arr[i][1]=int(arr[i][1])
 for i in range(n):
-    j=i+1
-    min=100000
-    min_index=0
-    min_name=''
+    j=i
+    min=101
     while j<n:
         if min>arr[j][1]:
             min=arr[j][1]
@@ -18,7 +16,9 @@ for i in range(n):
     temp=arr[i][1]
     arr[i][1]=min
     arr[min_index][1]=temp
+
     temp=arr[i][0]
     arr[i][0]=min_name
     arr[min_index][0]=temp
-    print(arr[i])
+for i in range(n):
+    print(arr[i][0])
